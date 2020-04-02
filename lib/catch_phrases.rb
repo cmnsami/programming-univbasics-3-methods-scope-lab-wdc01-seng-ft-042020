95d10abce1b986a67e66a9f6cebf73e1706dfb2d
Naseef Sami <cmnsami@gmail.com>
2020-04-02 15:18:54 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,13 +1,13 @@
 
 #status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
-def mario
-  phrase = "It's-a me, Mario!"
-  puts phrase
-  phrase
-end
+#def mario
+#  phrase = "It's-a me, Mario!"
+#  puts phrase
+#  phrase
+#end
 
-def toadstool
-  puts status
-end
+#def toadstool
+#  puts status
+#end
 
